8aab9eac33c443c9ed6b36e61d7b47feed01a064
Phoshoko Thabang <phoshokothabang1@gmail.com>
2023-10-19 18:10:27 +0200

    Added a singleRound function to determine a winner after a single round of the game

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ function getComputerChoice() {
     return choices[randomIndex]; 
 }
 
-computerSelection = getComputerChoice();
+function singleRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    }
+    //Player wins
+    else if (playerSelection === "Rock" & computerSelection === "Scissors") {
+        console.log("You Win! Rock beats Scissors");
+    }
+    else if (playerSelection === "Paper" & computerSelection === "Rock") {
+        console.log("You Win! Paper beats Rock");
+    }
+    else if (playerSelection === "Scissors" & computerSelection === "Paper") {
+        console.log("You Win! Scissors beats Paper");
+    }
+    //computer wins
+    else if (computerSelection === "Rock" & playerSelection === "Scissors") {
+        console.log("You Lose! Rock beats Scissors");
+    }
+    else if (computerSelection === "Paper" & playerSelection === "Rock") {
+        console.log("You Lose! Paper beats Rock");
+    }
+    else if (computerSelection === "Scissors" & playerSelection === "Paper") {
+        console.log("You Lose! Scissors beats Paper");
+    }
+    else {
+        console.log("Invalid Input\nMake sure you've selected one these: Rock, Paper, Scissors");
+    }
+}
