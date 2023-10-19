8014d55e19e128cf633c8bd417c5ffbcfec82d68
Phoshoko Thabang <phoshokothabang1@gmail.com>
2023-10-19 18:50:29 +0200

    changed the return values of singleRound function to give meaningful values and added a new function to play the game

--- script.js
+++ script.js
@@ -7,29 +7,71 @@ function getComputerChoice() {
 function singleRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return 0;
     }
     //Player wins
     else if (playerSelection === "Rock" & computerSelection === "Scissors") {
-        return "You Win! Rock beats Scissors";
+        return 1;
     }
     else if (playerSelection === "Paper" & computerSelection === "Rock") {
-        return "You Win! Paper beats Rock";
+        return 1;
     }
     else if (playerSelection === "Scissors" & computerSelection === "Paper") {
-        return "You Win! Scissors beats Paper";
+        return 1;
     }
     //computer wins
     else if (computerSelection === "Rock" & playerSelection === "Scissors") {
-        return "You Lose! Rock beats Scissors";
+        return 2;
     }
     else if (computerSelection === "Paper" & playerSelection === "Rock") {
-        return "You Lose! Paper beats Rock";
+        return 2;
     }
     else if (computerSelection === "Scissors" & playerSelection === "Paper") {
-        return "You Lose! Scissors beats Paper";
+        return 2;
     }
     else {
-        return "Invalid Input\nMake sure you've selected one these: Rock, Paper, Scissors";
+        return -1;
+    }
+}
+
+function game() {
+    //score 
+    let player = 0
+    let computer = 0
+
+    for (let i = 1; i <= 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("What is your selection: ");
+
+        const outcome = singleRound(playerSelection, computerSelection);
+        if (outcome === 1) {
+            player += 1;
+            console.log("Player: " + playerSelection,"\ncomputer: " + computerSelection);
+            console.log("You Win! " + playerSelection+ " beats " + computerSelection);
+        }
+        else if (outcome === 2) {
+            computer += 1;
+            console.log("Player: " + playerSelection,"\ncomputer: " + computerSelection);
+            console.log("You Lose! " + computerSelection+ " beats " + playerSelection);
+        }
+        else if (outcome === 0) {
+            
+            console.log("Player: " + playerSelection,"\ncomputer: " + computerSelection);
+            console.log("it's a tie"); 
+        }
+        else {
+            console.log("Invalid Input\nMake sure you've selected one these: Rock, Paper, Scissors")
+        }
+    }
+
+    console.log("Player: " + player,"\ncomputer: " + computer)
+    if (player === computer) {
+        console.log("It's a draw");
+    }
+    else if (player > computer) {
+        console.log("Congratulations! You won the game"); 
+    }
+    else {
+        console.log("You Lost, Better luck next time!");
     }
 }
